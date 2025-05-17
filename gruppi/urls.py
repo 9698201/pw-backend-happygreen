9fa8d84072fc2f060384acf3d2444618dbf160ac
bertoldi <9698201@itisrossi.vi.it>
2025-05-17 10:55:16 +0200

    prodotti

--- gruppi/urls.py
+++ gruppi/urls.py
@@ -7,8 +7,5 @@ router = DefaultRouter()
 router.register(r'', GruppoViewSet, basename='gruppo')
 
 urlpatterns = [
-    path('', views.GruppoListCreateView.as_view(), name='gruppi-list-create'),
-    path('iscrizione/', views.IscrizioneGruppoView.as_view(), name='iscrizione-gruppo'),
-    path('miei-gruppi/', views.MieiGruppiView.as_view(), name='miei-gruppi'),
     path('', include(router.urls)),
 ]
